8a81caef5395a294fef963bf924d3c75adbbe9d8
CesarOrdazV <cesar.ordazv@gmail.com>
2023-02-16 11:17:44 -0600

    Added a add item page

--- mercadito/item/forms.py
+++ mercadito/item/forms.py
@@ -1,7 +1,32 @@
 from django import forms
 from .models import Item
 
+INPUT_CLASES = 'w-full py-4 px-6 rounded-xl border-xl'
+
 class NewItemForm(forms.ModelForm):
     class Meta:
         model=Item
-        field=('category', 'name', 'description', 'price', 'image',)
+        fields=('category', 'name', 'description', 'price', 'image', 'inventory', 'date',)
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASES,
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASES,
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASES,
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASES,
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASES,
+            }),
+            'inventory': forms.TextInput(attrs={
+                'class': INPUT_CLASES,
+            }),
+            'date': forms.DateInput(attrs={
+                'class': INPUT_CLASES,
+            }),
+        }
