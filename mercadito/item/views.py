16c92766be89e8402de419967d875a9a2ddf915e
CesarOrdazV <cesar.ordazv@gmail.com>
2023-02-17 09:46:52 -0600

    Added delete and edit buttons

--- mercadito/item/views.py
+++ mercadito/item/views.py
@@ -31,3 +31,10 @@ def new(request):
         'form': form,
         'title': 'Nuevo artículo',
     })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, added=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
